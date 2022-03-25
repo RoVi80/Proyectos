0b43dbfef7194296b03553ae6f206cf5efc04ec7
RoVi80 <101873758+RoVi80@users.noreply.github.com>
2022-03-25 10:41:51 +0100

    Create test.py

---
+++ Python/conditional_iteration/fizz_buzz/test.py
@@ -0,0 +1,21 @@
+class PublicTestSuite(TestCase):
+
+    def test_one(self):
+        script.n = 1
+        res = script.fizz_buzz()
+        self.assertEqual(res, 1)
+
+    def test_three(self):
+        script.n = 3
+        res = script.fizz_buzz()
+        self.assertEqual(res, "Fizz")
+
+    def test_five(self):
+        script.n = 5
+        res = script.fizz_buzz()
+        self.assertEqual(res, "Buzz")
+
+    def test_fifteen(self):
+        script.n = 15
+        res = script.fizz_buzz()
+        self.assertEqual(res, "FizzBuzz")
