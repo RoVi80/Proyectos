046f230bcc44d76960baee447f2c1b056f34b39f
RoVi80 <101873758+RoVi80@users.noreply.github.com>
2022-03-29 09:17:23 +0200

    Add files via upload

---
+++ Inheritance/E10T3/cc088cb7-2ca3-35d5-bb33-af049587438b/public/onsite_restaurant.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Implement this class. Stick to the naming that is introduced in the
+# UML diagram. Do not change the class name or the method signatures
+# or the automated grading won't work.
+
+from public.restaurant import Restaurant
+
+class OnsiteRestaurant(Restaurant):
+
+    def __init__(self, name, cuisine_type, num_tables, is_open = False):
+        super().__init__(name, cuisine_type, is_open)
+        self.__num_tables = num_tables
+        self.__available_tables = num_tables
+
+    def occupy_table(self):
+        if self.__available_tables < 1:
+            raise Warning('No available tables')
+        else: self.__available_tables -= 1
+
+    def free_table(self):
+        if self.__available_tables == self.__num_tables:
+            raise Warning('All of the tables are free')
+        else: self.__available_tables += 1
+
+    def get_available_tables(self):
+        return self.__available_tables
